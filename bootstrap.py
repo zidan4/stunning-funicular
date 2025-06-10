c27a4dfce49afee466e42a7aab0a6ff66b9e9cef
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-06-10 21:36:02 +0300

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 
-
 data = datasets.load_wine(as_frame = True)
 
 X = data.data
